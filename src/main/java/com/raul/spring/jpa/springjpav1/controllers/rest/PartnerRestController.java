6e2d9f5edc4ac98db352db6a35636721dd94143e
RaulAbejonDelgado <raul.abejon.delgado@gmail.com>
2020-08-28 15:15:35 +0200

    Added upload partner photo endpoint

--- src/main/java/com/raul/spring/jpa/springjpav1/controllers/rest/PartnerRestController.java
+++ src/main/java/com/raul/spring/jpa/springjpav1/controllers/rest/PartnerRestController.java
@@ -2,19 +2,27 @@ package com.raul.spring.jpa.springjpav1.controllers.rest;
 
 import com.raul.spring.jpa.springjpav1.models.entity.Partner;
 import com.raul.spring.jpa.springjpav1.models.service.IPartnerRepoService;
+import com.raul.spring.jpa.springjpav1.models.service.IUploadService;
 import com.raul.spring.jpa.springjpav1.view.xml.PartnerList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -26,6 +34,11 @@ public class PartnerRestController {
 
     @Autowired
     private IPartnerRepoService partnerRepoService;
+
+    @Autowired
+    private IUploadService uploadService;
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
     @GetMapping(value = {"/partner/list-partner"})
     @Secured("ROLE_ADMIN")
@@ -124,6 +137,67 @@ public class PartnerRestController {
         return new ResponseEntity<Map<String,Object>>(response, HttpStatus.CREATED);
     }
 
+    @PutMapping("/partner-full-detail/{id}")
+    public ResponseEntity<?> partnerFullEdition(
+            //@Valid Partner partner,
+            @RequestParam("id")Long id,
+            @RequestParam(name = "file") MultipartFile photo
+    ) {
+        Partner partnerToUpdate = null;
+        Map<String, Object> response = new HashMap<>();
+
+        try{
+            partnerToUpdate = partnerRepoService.findOne(id);
+        } catch (DataAccessException e){
+            response.put("errors","Error updating the partner we can not look the partner");
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        if (!photo.isEmpty()) {
+            if (partnerToUpdate.getId() != null && partnerToUpdate.getId() > 0) {
+                try {
+                    partnerToUpdate = partnerRepoService.findOne(id);
+                    if (partnerToUpdate.getPhoto().length() > 0) {
+
+                        if (uploadService.delete(partnerToUpdate.getPhoto())) {
+                            response.put("warning", "Your old photo has been replace with : " + photo.getOriginalFilename());
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+        }
+        try {
+            partnerToUpdate.setPhoto(uploadService.copy(photo));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        response.put("success", "The partner was " + (partnerToUpdate.getId() != null ? "Updated" : "Created"));
+        partnerRepoService.save(partnerToUpdate);
+        response.put("partner",partnerToUpdate);
+
+        return new ResponseEntity<Map<String, Object>>(response , HttpStatus.CREATED) ;
+    }
+
+    @GetMapping(value = "/uploads/{filename:.+}")
+    public ResponseEntity<Resource> showPhoto(@PathVariable String filename) {
+        Resource resource = null;
+
+        try {
+            resource = uploadService.load(filename);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        HttpHeaders header = new HttpHeaders();
+        header.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename:\"" + resource.getFilename());
+        return new ResponseEntity<Resource>(resource,header,HttpStatus.OK);
+
+    }
+
     @DeleteMapping("/partner/{id}")
     public ResponseEntity<?> deletePartner(@PathVariable Long id) {
         Map<String, Object> response = new HashMap<>();
@@ -140,4 +214,5 @@ public class PartnerRestController {
         return new ResponseEntity<Map<String,Object>>(response, HttpStatus.OK);
 
     }
+
 }
